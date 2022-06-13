1d2e1e277b883e05f46496c2f801682611ce86bb
Ote Leo <ote.leo73@gmail.com>
2022-06-13 15:52:23 +0200

    Transaction Boiler Plate Code

--- blockchain/src/Transaction.java
+++ blockchain/src/Transaction.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Optional;
 
 /**
@@ -5,49 +6,84 @@ import java.util.Optional;
  * The data structure to hold the transaction
  */
 public final class Transaction {
-    private final String PREVIOUS_HASH;
-    private final String SIGNATURE;
+    private final String[] data;
+    private final String signature;
+    private final String previousHash;
     private final Optional<String> keyPair;
-    private final String[] DATA;
 
-    public Transaction(String previoushash, String signatrue, String[] data) {
-        PREVIOUS_HASH = previoushash;
-        DATA = data;
-        SIGNATURE = signatrue;
-        // In haskell -> MONAD
-        keyPair = Optional.empty();
+    public Transaction(String previousHash, String signature, String[] data) {
+        this.previousHash = previousHash;
+        this.signature = signature;
+        this.data = data;
+        this.keyPair = Optional.empty();
     }
 
-    public Transaction(String previoushash, String signatrue, String[] data, Optional<String> pair) {
-        PREVIOUS_HASH = previoushash;
-        DATA = data;
-        SIGNATURE = signatrue;
-        keyPair = pair;
+    public Transaction(String previousHash, String signature, String[] data, Optional<String> keyPair) {
+        this.previousHash = previousHash;
+        this.signature = signature;
+        this.keyPair = keyPair;
+        this.data = data;
     }
 
     public String[] getData() {
-        return DATA;
+        return this.data;
+    }
+
+    public String getSignature() {
+        return signature;
     }
 
     public String getPreviousHash() {
-        return PREVIOUS_HASH;
+        return previousHash;
     }
 
-    /**
-     * Only encrypt the data\
-     * Do not encrypt the signatures
-     * 
-     * @return
-     */
-    public String encrypt() {
-        return null;
+    public Optional<String> getKeyPair() {
+        return keyPair;
     }
 
-    public String decrypt() {
-        return null;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(data);
+        result = prime * result + ((keyPair == null) ? 0 : keyPair.hashCode());
+        result = prime * result + ((previousHash == null) ? 0 : previousHash.hashCode());
+        result = prime * result + ((signature == null) ? 0 : signature.hashCode());
+        return result;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Transaction other = (Transaction) obj;
+        if (!Arrays.equals(data, other.data))
+            return false;
+        if (keyPair == null) {
+            if (other.keyPair != null)
+                return false;
+        } else if (!keyPair.equals(other.keyPair))
+            return false;
+        if (previousHash == null) {
+            if (other.previousHash != null)
+                return false;
+        } else if (!previousHash.equals(other.previousHash))
+            return false;
+        if (signature == null) {
+            if (other.signature != null)
+                return false;
+        } else if (!signature.equals(other.signature))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
-        return String.format("TRANSACTION [%s]");
+        return String.format("TRANSACTION [DATA = %s, KEYPAIR = %s, PREVIOUS HASH = %s, SIGNATURE = %s]",
+                Arrays.toString(data), keyPair, previousHash, signature);
     }
 }
