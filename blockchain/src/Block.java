5758251e3001ddd808f7fb0231a07746fc9b187a
Ote Leo <ote.leo73@gmail.com>
2022-06-13 17:55:40 +0200

    Adding boiler plate code

--- blockchain/src/Block.java
+++ blockchain/src/Block.java
@@ -11,7 +11,7 @@ import security.DukeHash;
 public class Block {
     private static final int TRANSACTIONAL_CAPACITY = 3;
 
-    private static final String NULL_MINE_TIMESTAMP_ERR = "BLOCK `%s' HAVE NOT BEEN MINED YET!";
+    private static final String NULL_MINE_TIMESTAMP_ERR = "BLOCK (UUID: `%s', MEM: `%s') HAVE NOT BEEN MINED YET!";
     private static final String FULL_BLOCK_ERR = "BLOCK `%s' IS FULL (BLOCK CAPACITY IS `%d')";
 
     private final List<Transaction> transactions;
@@ -38,7 +38,7 @@ public class Block {
 
     public Timestamp getMiningTimeStamp() throws IllegalStateException {
         if (miningTimestamp == null)
-            throw new IllegalStateException(String.format(Block.NULL_MINE_TIMESTAMP_ERR, this));
+            throw new IllegalStateException(String.format(Block.NULL_MINE_TIMESTAMP_ERR, uuid.toString(), this));
         return miningTimestamp;
     }
 
@@ -56,16 +56,61 @@ public class Block {
         return DukeHash.hash(blockBody);
     }
 
-    // public String getPreviousHash() {
-    // return PREVIOUS_HASH;
-    // }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((miningTimestamp == null) ? 0 : miningTimestamp.hashCode());
+        result = prime * result + (int) (nonce ^ (nonce >>> 32));
+        result = prime * result + ((previousHash == null) ? 0 : previousHash.hashCode());
+        result = prime * result + ((transactions == null) ? 0 : transactions.hashCode());
+        result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Block other = (Block) obj;
+        if (miningTimestamp == null) {
+            if (other.miningTimestamp != null)
+                return false;
+        } else if (!miningTimestamp.equals(other.miningTimestamp))
+            return false;
+        if (nonce != other.nonce)
+            return false;
+        if (previousHash == null) {
+            if (other.previousHash != null)
+                return false;
+        } else if (!previousHash.equals(other.previousHash))
+            return false;
+        if (transactions == null) {
+            if (other.transactions != null)
+                return false;
+        } else if (!transactions.equals(other.transactions))
+            return false;
+        if (uuid == null) {
+            if (other.uuid != null)
+                return false;
+        } else if (!uuid.equals(other.uuid))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("BLOCK (%s) [MINING TIMESTAMP = %s, NONCE = %s, PREVIOUS HASH = %s, TRANSACTIONS = %s]",
+                uuid.toString(), (miningTimestamp == null ? "null" : miningTimestamp.toString()), Long.toString(nonce),
+                previousHash, transactions.toString());
+    }
 
     // public void mine() {
 
-    // }
-
-    // public long getNonce() {
-    // return nonce;
     // }
 
     // private String encryptTransaction() {
