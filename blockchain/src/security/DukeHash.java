31d1255034d968f79b8f8282c1b54ab2cd8f28b8
Ote Leo <ote.leo73@gmail.com>
2022-06-13 16:56:53 +0200

    Adding SHA-256' Hashing

---
+++ blockchain/src/security/DukeHash.java
@@ -0,0 +1,25 @@
+package security;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * The Hashing class of the blockchain; hashing algorithm is set to SHA256
+ */
+public class DukeHash {
+    private static final String HASHING_ALGORITHM = "SHA-256";
+    private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
+    public static String hash(String message) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(HASHING_ALGORITHM);
+            return CryptoUtils.hexRepresentation(messageDigest.digest(message.getBytes(UTF_8)));
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("SOME IS REALLy WRONG!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
